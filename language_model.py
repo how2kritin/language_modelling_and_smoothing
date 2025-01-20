a20b2c12895dcd5f9ad649fff2bfe40fef3a6b1e
how2kritin <kritinmaddired@gmail.com>
2025-01-20 16:53:48 +0530

    fix: Generalized the probability computation function in LinearInterpolation to use that of the NGramModels.

--- language_model.py
+++ language_model.py
@@ -118,7 +118,7 @@ class NGramModel:
 
         return ((r + 1) * S_Nr_plus_1) / S_Nr
 
-    def _calculate_probability(self, ngram: tuple[str, ...], context: tuple[str, ...]) -> float:
+    def calculate_probability(self, ngram: tuple[str, ...], context: tuple[str, ...]) -> float:
         """
         A general probability calculation function to handle various smoothing methods.
         :param ngram: The ngram whose probability we wish to calculate.
@@ -173,7 +173,7 @@ class NGramModel:
         predictions = {}
         for ngram in self.ngrams:
             if ngram[:-1] == context:
-                probability = self._calculate_probability(ngram, context)
+                probability = self.calculate_probability(ngram, context)
                 predictions[ngram[-1]] = probability
 
         # return the top n_next_words after sorting them by decreasing order of probabilities.
@@ -193,7 +193,7 @@ class NGramModel:
         for i in range(len(tokens) - self.n + 1):
             ngram = tuple(tokens[i:i + self.n])
             context = tuple(tokens[i:i + self.n - 1])
-            probability *= self._calculate_probability(ngram, context)
+            probability *= self.calculate_probability(ngram, context)
 
         return probability
 
@@ -206,7 +206,7 @@ class NGramModel:
         """
         for _ in range(n):
             next_words_list = list(self.predict_next_word(sentence, 1).keys())
-            if not next_words_list: # if there is no word that can come next as this context hasn't been seen before
+            if not next_words_list:  # if there is no word that can come next as this context hasn't been seen before
                 break
             next_word = next_words_list[0]
 
@@ -273,8 +273,9 @@ class LinearInterpolationOfNGramModels:
                 sub_context = sub_ngram[:-1]  # take all but the last token
 
                 if k == 1:  # unigram case
+                    unigram_count = self.ngram_models[0].ngrams.get(sub_ngram, 0)
                     if N > 1:
-                        values.append((count - 1) / (N - 1))
+                        values.append((unigram_count - 1) / (N - 1))
                     else:
                         values.append(0.0)
                 else:
@@ -297,31 +298,18 @@ class LinearInterpolationOfNGramModels:
             # just fallback to equal weights if no data (i.e., total of all lambdas came out to be 0 because each of them remained 0).
             self.lambdas = [1.0 / self.n] * self.n
 
-    def _calculate_probability(self, ngram: tuple[str, ...]) -> float:
+    def calculate_probability(self, ngram: tuple[str, ...]) -> float:
         if self.lambdas is None:  # this should be done during training. however, we are doing it here as well, for the sake of completeness.
             self._calculate_linear_interpolation_weights()
 
         probability = 0.0
-        N = sum(self.ngram_models[0].ngrams.values())  # total number of tokens in corpus (total number of unigrams)
 
         # for each k from 1 to n
         for k in range(1, self.n + 1):
             sub_ngram = ngram[-k:]  # take the last k tokens
             sub_context = sub_ngram[:-1]  # take all but the last token
 
-            if k == 1:  # unigram case
-                if N > 0:
-                    prob = self.ngram_models[0].ngrams.get(sub_ngram, 0) / N
-                else:
-                    prob = 0.0
-            else:
-                sub_count = self.ngram_models[k - 1].ngrams.get(sub_ngram, 0)
-                context_count = self.ngram_models[k - 1].context_counts.get(sub_context, 0)
-                if context_count > 0:
-                    prob = sub_count / context_count
-                else:
-                    prob = 0.0
-
+            prob = self.ngram_models[k - 1].calculate_probability(sub_ngram, sub_context)
             probability += self.lambdas[k - 1] * prob
 
         return probability
@@ -348,7 +336,7 @@ class LinearInterpolationOfNGramModels:
 
                     # calculate probability using linear interpolation
                     full_ngram = context + (next_word,)
-                    predictions[next_word] = self._calculate_probability(full_ngram)
+                    predictions[next_word] = self.calculate_probability(full_ngram)
 
         # return the top n_next_words after sorting them by decreasing order of probabilities.
         sorted_predictions = dict(
@@ -366,7 +354,7 @@ class LinearInterpolationOfNGramModels:
 
         for i in range(len(tokens) - self.n + 1):
             ngram = tuple(tokens[i:i + self.n])
-            prob = self._calculate_probability(ngram)
+            prob = self.calculate_probability(ngram)
             probability *= prob
 
         return probability
@@ -380,7 +368,7 @@ class LinearInterpolationOfNGramModels:
         """
         for _ in range(n):
             next_words_list = list(self.predict_next_word(sentence, 1).keys())
-            if not next_words_list: # if there is no word that can come next as this context hasn't been seen before
+            if not next_words_list:  # if there is no word that can come next as this context hasn't been seen before
                 break
             next_word = next_words_list[0]
 
